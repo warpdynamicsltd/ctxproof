fd1dedc11721dcdd8ccba1fc5d446731f67f5805
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-11-01 23:12:14 +0100

    Simplify `parse_string` function and extend test cases for `Pred` parsing.

--- CtxProof/test/test_CtxProof.ml
+++ CtxProof/test/test_CtxProof.ml
@@ -1,13 +1,7 @@
 open Ctxproof
 
 let parse_string s =
-  let lexbuf = 
-    try 
-      Lexing.from_string s
-    with
-      | _ -> Printf.eprintf "aaa"; exit 1;
-
-  in
+  let lexbuf =  Lexing.from_string s in
   Parser.input Lexer.token lexbuf
 
 let () =
@@ -15,6 +9,7 @@ let () =
   assert (parse_string("$false") = False);
   assert (parse_string("p") = Pred ("p", []));
   assert (parse_string("p(X)") = Pred ("p", [Var "X"]));
+  assert (parse_string("p(X,Y)") = Pred ("p", [Var "X"; Var "Y"]));
   assert (parse_string("p(c)") = Pred ("p", [Const "c"]));
   assert (parse_string("p(f(X))") = Pred ("p", [Func("f", [Var "X"])]));
   assert (parse_string("p(f(X),g(X))") = Pred ("p", [Func("f", [Var "X"]); Func("g", [Var "X"])]));

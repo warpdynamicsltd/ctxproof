1e4529be5dab8ebb4290b54d6147df7a52ed2b5b
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-11-03 20:14:11 +0100

    Extend `test_fof_negative.ml` with 100 additional comprehensive invalid formula test cases.

--- CtxProof/test/parser/test_fof_negative.ml
+++ CtxProof/test/parser/test_fof_negative.ml
@@ -18,3 +18,208 @@ let run () =
   assert (not_wff "p & p & p");
   assert (not_wff "p & p | p");
   assert (not_wff "p => p | p");
+
+  (* 100 additional negative tests *)
+  assert (not_wff "");
+  assert (not_wff "   ");
+  assert (not_wff "(");
+  assert (not_wff ")");
+  assert (not_wff "()");
+
+  (* Unbalanced parentheses *)
+  assert (not_wff "(p");
+  assert (not_wff "p)");
+  assert (not_wff "(p & q");
+  assert (not_wff "p & q)");
+  assert (not_wff "((p)");
+  assert (not_wff "p))");
+  assert (not_wff "(p | (q & r)");
+  assert (not_wff "((p | q) & r");
+
+  (* Dangling operators *)
+  assert (not_wff "&");
+  assert (not_wff "|");
+  assert (not_wff "=>");
+  assert (not_wff "<=>");
+  assert (not_wff "~");
+  assert (not_wff "p &");
+  assert (not_wff "& p");
+  assert (not_wff "p |");
+  assert (not_wff "| p");
+  assert (not_wff "p =>");
+  assert (not_wff "=> p");
+  assert (not_wff "p <=>");
+  assert (not_wff "<=> p");
+  assert (not_wff "~");
+
+  (* Double/tandem unary *)
+  assert (not_wff "~~");
+  assert (not_wff "~& p");
+  assert (not_wff "~| p");
+  assert (not_wff "~=> p");
+  assert (not_wff "~~ => p");
+
+  (* Bad quantifier headers *)
+  assert (not_wff "! X: p(X)");
+  assert (not_wff "? X: p(X)");
+  assert (not_wff "! []: p(X)");
+  assert (not_wff "? []: p(X)");
+  assert (not_wff "! [: p(X)");
+  assert (not_wff "? ]: p(X)");
+  assert (not_wff "! [ ]: p(X)");
+  assert (not_wff "! [X Y]: p(X)");
+  assert (not_wff "? [X,]: p(X)");
+  assert (not_wff "! [,X]: p(X)");
+  assert (not_wff "! [x]: p(x)");      (* lowercase var not allowed if your lexer enforces UWORD *)
+  assert (not_wff "! [X]:");
+  assert (not_wff "! : p(X)");
+  assert (not_wff "! [X] p(X)");       (* missing colon *)
+  assert (not_wff "? [X] p(X)");       (* missing colon *)
+  assert (not_wff "! [X]:");
+  assert (not_wff "? [X]:");
+  assert (not_wff "! [X]: ~");
+  assert (not_wff "? [X]: & p(X)");
+  (* Malformed terms/predicates *)
+  assert (not_wff "p(");
+  assert (not_wff "p)");
+  assert (not_wff "p(,)");
+  assert (not_wff "p(,X)");
+  assert (not_wff "p(X,)");
+  assert (not_wff "p(X,,Y)");
+  assert (not_wff "p(,X,Y)");
+  assert (not_wff "p(X,Y,)");
+  assert (not_wff "p(()");
+  assert (not_wff "p())");
+  assert (not_wff "p(()))");
+  assert (not_wff "p((X)");
+  assert (not_wff "p(X))");
+  assert (not_wff "p((X,)Y)");
+  assert (not_wff "p(X Y)");
+  assert (not_wff "p(X;Y)");
+  assert (not_wff "p[X]");
+  assert (not_wff "p{X}");
+  assert (not_wff "p<X>");
+
+  (* Equality malformed *)
+  assert (not_wff "=");
+  assert (not_wff "=");
+  assert (not_wff "= X");
+  assert (not_wff "X =");
+  
+  assert (not_wff "(=)");
+  assert (not_wff "(X=)");
+  assert (not_wff "(=Y)");
+  assert (not_wff "=(X,Y)");
+  assert (not_wff "X == Y");
+  assert (not_wff "X = = Y");
+  assert (not_wff "X = ( )");
+
+  (* Mixed operator precedence mistakes *)
+  assert (not_wff "(p & ) q");
+  assert (not_wff "p ( & q )");
+  assert (not_wff "( p | )");
+  assert (not_wff "( ~ )");
+  assert (not_wff "~()");
+  assert (not_wff "~( )");
+  assert (not_wff "~(p & )");
+  assert (not_wff "~( & p)");
+  assert (not_wff "(p <=> )");
+  assert (not_wff "( => p)");
+
+  (* Nested term errors *)
+  assert (not_wff "p(f())");           (* empty arg list to function if not allowed *)
+  assert (not_wff "p(f(,X))");
+  assert (not_wff "p(f(X,))");
+  assert (not_wff "p(f(,))");
+  assert (not_wff "p(f(,g(X)))");
+  assert (not_wff "p(f(g(),X))");      (* empty g() if functions require args *)
+  assert (not_wff "p(,)");
+  assert (not_wff "p((,))");
+
+  (* Variable placement mistakes *)
+  assert (not_wff "q(1)");             (* numeric constant if banned *)
+  assert (not_wff "q('a)");            (* quoted atom if not in spec *)
+  assert (not_wff "q(\"a\")");         (* strings not allowed *)
+  assert (not_wff "p(X Y Z)");         (* missing commas *)
+  assert (not_wff "p(X,,Z)");
+
+  (* Chained binaries without parentheses where grammar forbids *)
+  assert (not_wff "p => q => r");
+  assert (not_wff "p <=> q <=> r");
+  assert (not_wff "p & q |");
+  assert (not_wff "| p & q");
+
+  (* Quantifier variable lists malformed *)
+  assert (not_wff "! [X,,Y]: p(X,Y)");
+  assert (not_wff "! [,]: p()");
+  assert (not_wff "? [,,]: p()");
+  assert (not_wff "! [X,,]: p(X)");
+  assert (not_wff "? [,X,]: p(X)");
+  assert (not_wff "! [X,Y,]: p(X,Y)");
+  assert (not_wff "? [,X,Y]: p(X,Y)");
+
+  (* Bad tokens around quantifiers *)
+  assert (not_wff "! [X]:: p(X)");
+  assert (not_wff "? [X]:: p(X)");
+  assert (not_wff "! [[X]]: p(X)");
+  assert (not_wff "? [[X]]: p(X)");
+
+  (* Misplaced quantifiers in expressions *)
+  assert (not_wff "p(X) ! [Y]: q(Y)");
+  assert (not_wff "p(X) ? [Y]: q(Y)");
+  assert (not_wff "( ! [X]: p(X) ) q(Y)");
+  assert (not_wff "( ? [X]: p(X) ) q(Y)");
+
+  (* Operators where terms are expected *)
+  assert (not_wff "p(&)");
+  assert (not_wff "p(|)");
+  assert (not_wff "p(=>)");
+  assert (not_wff "p(<=>)");
+  assert (not_wff "p(~)");
+  assert (not_wff "p(p=>q)");
+
+  (* Trailing commas and separators *)
+  assert (not_wff "p(,)");
+  assert (not_wff "p(X,,)");
+  assert (not_wff "p(,X,)");
+  assert (not_wff "p(, ,)");
+  assert (not_wff "p(X, ,Y)");
+
+  (* Parentheses misuse with binaries *)
+  assert (not_wff "(p) (q)");
+  assert (not_wff "(p) q");
+  assert (not_wff "(p) &");
+  assert (not_wff "& (p)");
+  assert (not_wff "(p) |");
+  assert (not_wff "| (p)");
+
+  (* Equality with non-terms *)
+  assert (not_wff "p() = q()");     
+
+  (* Mixed stray punctuation *)
+  assert (not_wff "p, q");
+  assert (not_wff "p; q");
+  assert (not_wff "p: q");
+  assert (not_wff "p@q");
+  assert (not_wff "p#q");
+  assert (not_wff "p$q");
+
+  (* Bad nesting with quantifiers and parens *)
+  assert (not_wff "! [X: p(X))");
+  assert (not_wff "! [X): p(X)");
+  assert (not_wff "? [X: (p(X))");
+  assert (not_wff "? [X): (p(X))");
+  assert (not_wff "! X]: p(X)");
+  assert (not_wff "? X]: p(X)");
+  assert (not_wff "! [X Y Z]: p(X,Y,Z)");
+  assert (not_wff "? [X Y Z]: p(X,Y,Z)");
+
+  (* Misordered equality *)
+  assert (not_wff "=X");
+  assert (not_wff "X=");
+  assert (not_wff "(= X Y)");
+  assert (not_wff "=(X)=Y");
+
+  (* Random tptp-like but invalid in your subset *)
+  assert (not_wff "$true &");
+  assert (not_wff "$false |");

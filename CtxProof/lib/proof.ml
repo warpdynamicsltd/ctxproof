7bca1dca2755f57adcc8370bdb2e815f04b3f688
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-11-27 20:53:00 +0100

    Introduce `RefPosError` for detailed reference position error handling, implement `ref_system_is_correct` for hierarchical reference validation, enhance proof validation logic with stricter reference checks, and update error messaging for clarity.

--- CtxProof/lib/proof.ml
+++ CtxProof/lib/proof.ml
@@ -1,16 +1,41 @@
+open Types
 open Kernel
 open Parser_utils
 
 exception ProofError of string
 
+exception RefPosError of string * Lexing.position
+
+
+let append_int_to_ref ref i = 
+  match ref with 
+    | Ref lst -> Ref (lst @ [Z.of_int i])
+
+
+let rec ref_system_is_correct statement root_ref = 
+  match statement with 
+    | Statement {ref; statements; _} when ref = root_ref
+        ->
+          List.for_all (Fun.id)
+          (List.mapi 
+            (
+              fun i s -> (ref_system_is_correct s (append_int_to_ref root_ref i))
+            ) 
+          statements)
+    | Statement {pos;_} -> raise (RefPosError ("invalid reference", pos))
+
+
 let proved proof ref = 
   try
-    prove_thesis proof ref
+    ref_system_is_correct proof (Ref [])
+    && prove_thesis proof ref;
   with 
+    | RefPosError (msg, pos)
     | KernelPosError (msg, pos)
       -> raise (ProofError (msg ^ " " ^ (location_to_string pos)))
     | KernelError msg
       -> raise (ProofError msg)
     
     
+
 let valid proof = proved proof (Ref [])

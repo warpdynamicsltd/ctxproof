f5c93b58ddb55d2ca70f2eb36ce0f297880ee1ce
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-11-30 22:46:13 +0100

    Standardize indentation and add missing semicolons in `test_main.ml`.

--- CtxProof/test/test_main.ml
+++ CtxProof/test/test_main.ml
@@ -1,8 +1,9 @@
 let () =
-  Test_fof_positive.run();
-  Test_fof_negative.run();
-  Test_line.run();
-  Test_proof.run();
-  Test_logic_ops.run();
-  
-  print_endline "All tests passed!" 
+
+Test_fof_positive.run();
+Test_fof_negative.run();
+Test_line.run();
+Test_proof.run();
+Test_logic_ops.run();
+
+print_endline "All tests passed!";
